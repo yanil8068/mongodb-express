48e884bf0ff61f638b85c8fd96d21dd1a3c3f6ca
yanil8068 <yanil8068@gmail.com>
2024-01-01 10:02:22 +0530

    index route

--- index.js
+++ index.js
@@ -4,17 +4,6 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 
-let chat1 = new Chat({
-    from: "neha",
-    to: "priya",
-    msg: "send me your exam sheets",
-    created_at: new Date(),
-});
-chat1.save().then((res)=>{
-    console.log(res);
-});
-
-
 main().then(() => {
     console.log("connection successful");
 })
@@ -27,9 +16,19 @@ app.listen(8080, ()=>{
     console.log("server is listening on port 8080");
 })
 
+app.use(express.static(path.join(__dirname,"public")));
+
+// index route
+app.get("/chats", async(req, res)=>{
+    let chats = await Chat.find();
+    console.log(chats);
+    res.render("index.ejs",{chats});
+});
+
 app.get("/", (req, res)=>{
     res.send("root is working");
 })
+
 
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine", "ejs");
