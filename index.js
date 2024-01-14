9bdfc3139d45d4bbb6bbabcdd2b853c36966c59f
yanil8068 <yanil8068@gmail.com>
2024-01-14 21:46:22 +0530

    Edit Route

--- index.js
+++ index.js
@@ -57,3 +57,11 @@ app.post("/chats",(req, res)=>{
     });
     res.redirect("/chats");
 });
+
+
+//Edit route
+app.get("/chats/:id/edit" ,async(req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+})
