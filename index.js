ce19b314cac6e07a4556733f76473d286d678da8
yanil8068 <yanil8068@gmail.com>
2024-01-23 13:17:45 +0530

    destroy route

--- index.js
+++ index.js
@@ -83,3 +83,11 @@ app.get("/chats/:id/edit" ,async(req,res)=>{
     let chat = await Chat.findById(id);
     res.render("edit.ejs",{chat});
 })
+
+//destroy route
+app.delete("/chats/:id",async(req, res)=>{
+    let {id} = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chats");
+});
